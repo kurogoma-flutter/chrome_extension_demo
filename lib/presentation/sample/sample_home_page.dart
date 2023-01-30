3ffb7d79e8d862e8ceaff5e9b839c23ace8199d9
Kurogoma939 <kan.sss.939@gmail.com>
2023-01-31 00:40:57 +0900

    データ取得サンプル

--- lib/presentation/sample/sample_home_page.dart
+++ lib/presentation/sample/sample_home_page.dart
@@ -25,20 +25,146 @@ class SampleHomePage extends HookConsumerWidget {
     // Widget Style
 
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 50, 50, 50),
       appBar: AppBar(
-        title: const Text('Sample Home Page'),
+        title: const Text('Customer Name'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Sample Home Page',
-              style: Theme.of(context).textTheme.headline4,
+      body: state.sampleList.isEmpty
+          ? const Center(
+              child: Text(
+                'Yoriaiサービス対象外です',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Color.fromARGB(255, 8, 67, 232),
+                ),
+              ),
+            )
+          : ListView(
+              children: [
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Row(
+                      children: const [
+                        Icon(Icons.paid_rounded),
+                        Text('ラベル1'),
+                      ],
+                    ),
+                    subtitle: const Text('detail explain sample'),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
     );
   }
 }
